25bede9d336cd6e9f4c93191b40cbf0a32e7b1d1
Giorgio <giorgio.pignattari@gmail.it>
2021-03-24 15:37:23 +0100

    inizio js

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// <!-- Descrizione
+// Rifare l'esercizio dello slider come fatto assieme in classe.
+// Bonus
+// Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente. -->
+
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+
+    }
+  }
+);
