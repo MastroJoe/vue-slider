39a40512767590734734090171740a2ea2ffd2bc
Giorgio <giorgio.pignattari@gmail.it>
2021-03-24 16:28:49 +0100

    inserimento imgs e icone

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,12 @@ var app = new Vue(
   {
     el: '#root',
     data: {
-
+      index: 0,
+      imgs: [
+        'https://www.wallpapertip.com/wmimgs/3-35636_download-nature-wallpapers-full-hd-1080p-photo-desktop.jpg',
+        'https://i.pinimg.com/originals/25/2d/e9/252de9410dba461a7f27d9b7eebad0e2.jpg',
+        'https://wallpapercave.com/wp/wp2809570.jpg',
+      ]
     }
   }
 );
